3f5ad06236187583e97601d58279f52dd6554faf
M999FJB <Fahdi.Jbeli@mutworld.be>
2025-04-05 09:17:25 +0200

    Refactor conditions related that test on sulfuras item

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -53,19 +53,17 @@ class GildedRose {
                     item.quality = 0;
                 }
             } else {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                boolean isSulfuras = item.name.equals("Sulfuras, Hand of Ragnaros");
+
+                if (!isSulfuras) {
+                    if (item.quality > 0) {
                         item.quality = item.quality - 1;
                     }
-                }
 
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                     item.sellIn = item.sellIn - 1;
-                }
 
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (item.sellIn < 0) {
+                        if (item.quality > 0) {
                             item.quality = item.quality - 1;
                         }
                     }
