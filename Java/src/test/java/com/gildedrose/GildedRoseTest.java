4da3e30b17175d8c29800e13381b2b60495c70dd
M999FJB <Fahdi.Jbeli@mutworld.be>
2025-04-05 12:39:44 +0200

    Adapt the test and increase the code coverage to 100%

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,36 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    @ParameterizedTest
+    @MethodSource("provideItemsForTest")
+    void testUpdate(Item item, String expected) {
+        Item[] items = new Item[]{item};
         GildedRose app = new GildedRose(items);
         app.update();
-        assertEquals("foo", app.items[0].name);
+        assertEquals(expected, app.items[0].toString());
+    }
+
+    private static Stream<Arguments> provideItemsForTest() {
+        return Stream.of(
+            Arguments.of(new Item("foo", 0, 0), "foo, -1, 0"),
+            Arguments.of(new Item("Conjured Mana Cake", 3, 6), "Conjured Mana Cake, 2, 5"),
+            Arguments.of(new Item("Aged Brie", 2, 0), "Aged Brie, 1, 1"),
+            Arguments.of(new Item("Aged Brie", 0, 0), "Aged Brie, -1, 2"),
+            Arguments.of(new Item("Sulfuras, Hand of Ragnaros", 0, 80), "Sulfuras, Hand of Ragnaros, 0, 80"),
+            Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20), "Backstage passes to a TAFKAL80ETC concert, 14, 21"),
+            Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49), "Backstage passes to a TAFKAL80ETC concert, 9, 50"),
+            Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49), "Backstage passes to a TAFKAL80ETC concert, 4, 50"),
+            Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49), "Backstage passes to a TAFKAL80ETC concert, -1, 0")
+        );
     }
 
 }
