49d0f95a5f6103ecf38ed43c6d807e8c2c339079
M999FJB <Fahdi.Jbeli@mutworld.be>
2025-04-05 09:06:55 +0200

    Refactor logic of updating quality and sellIn related to backstage item into one condition

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -15,7 +15,7 @@ class GildedRose {
 
     private void updateQualityItem(Item item) {
         boolean isAgedBrie = item.name.equals("Aged Brie");
-        if(isAgedBrie) {
+        if (isAgedBrie) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
 
@@ -29,13 +29,8 @@ class GildedRose {
                 }
             }
         } else {
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
+            boolean isBackstage = item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+            if (isBackstage) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
@@ -51,23 +46,32 @@ class GildedRose {
                         }
                     }
                 }
-            }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 item.sellIn = item.sellIn - 1;
-            }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
+            } else {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.sellIn = item.sellIn - 1;
+                }
+
+                if (item.sellIn < 0) {
                     if (item.quality > 0) {
                         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                             item.quality = item.quality - 1;
                         }
                     }
-                } else {
-                    item.quality = 0;
                 }
             }
+
         }
 
     }
